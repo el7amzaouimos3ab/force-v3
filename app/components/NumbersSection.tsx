3ca7c19523ebd7446dbbd8c3ea5dfb674e1ab080
mossaabe <el7amzaouimos3ab@gmail.com>
2025-02-16 04:06:20 +0100

    test

--- app/components/NumbersSection.tsx
+++ app/components/NumbersSection.tsx
@@ -73,7 +73,7 @@ const NumbersSection = () => {
   return (
     <>
 
-    <section className="numbers-section bg-[#C13C27]">
+    <section className="numbers-section h-[100%] bg-[#C13C27]">
 
     <div className="grid grid-cols-1 md:grid-cols-3 h-1/2">
       {/* Grid Item 1 */}
--- app/components/ZoomingLogo.tsx
+++ app/components/ZoomingLogo.tsx
@@ -12,53 +12,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default function ZoomingLogo() {
 
-useEffect(()=> {
 
-    
-    
-
-    ScrollTrigger.create({
-
-        trigger: ".main",
-        start:"top top",
-        end: ()=> `+=${innerHeight * 2}`,
-        pin: true,
-        pinSpacing: true,
-
-    })
-
-    
-
-
-   gsap.to(".logo-logo", {
-      scrollTrigger: {
-        trigger: ".logo-logo", // This triggers the effect when the logo enters the viewport
-        start: "top top", // Trigger when top of the logo hits the bottom of the viewport
-        end: `+=${window.innerHeight }`, // End when top of the logo reaches top of the viewport
-        scrub: true, // Makes the animation smooth with scrolling
-
-    },
-    ease: "none",
-    scale: 25, // Scale the logo to 25x its size (adjust as needed)
-    });
-
-    gsap.to(".final-section", {
-        scrollTrigger: {
-          trigger: ".final-section", // This triggers the effect when the logo enters the viewport
-          start: `+=${window.innerHeight }`, // Trigger when top of the logo hits the bottom of the viewport
-          end: `top top`, // End when top of the logo reaches top of the viewport
-          scrub: true, // Makes the animation smooth with scrolling
-  
-      },
-        opacity: 1,
-        y: 0,
-        filter: "blur(0px)",
-        duration: 0.5,
-      });
-
-
-
-}, []);
 
 
   return (
