0361bced708d7fafeeadbe639e9939b1a935f784
mossaabe <el7amzaouimos3ab@gmail.com>
2025-02-17 08:54:31 +0100

    zoom

--- app/components/ZoomingLogo.tsx
+++ app/components/ZoomingLogo.tsx
@@ -60,27 +60,24 @@ export default function ZoomingLogo() {
 
   return (
     <>
-        
-        <section className='main bg-gray-800 flex items-center justify-center w-full h-full overflow-hidden'>
-            
-
+        <div className=" bg-black w-full h-[300vh]">
+        <section className='main flex items-center justify-center w-full h-[100vh] overflow-hidden'>
             <Image 
               id="logo"
               src="/logos/logoo.svg" // You can also use import to get a local image.
               alt="Image 1" 
               width={200} 
               height={200} 
-              className="logo origin-[50%_43%]"
+              className="logo origin-[50%_43%] relative"
             />
-
             <div className='absolute flex justify-center items-center text-center'> 
                 
                 <h1 className='text-zoom text-3xl md:text-4xl w-[95%] md:w-[70%] '>نحن شغوفون بالابتكار والأفكار الرائعة والتنفيذ الذي يجمع كل ذلك في تجربة واحدة جميلة. إذا كنت كذلك، فاتصل بنا أو أرسل لنا بريدًا إلكترونيًا للبدء. </h1>
-            
+                 
             </div>
 
         </section>
-        
+        </div>
         
     </>
   )
