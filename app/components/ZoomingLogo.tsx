79c3ae0121ad7c93fa9ae305f43e91d731c38384
mossaabe <el7amzaouimos3ab@gmail.com>
2025-02-23 01:18:13 +0100

    zoom

--- app/components/ZoomingLogo.tsx
+++ app/components/ZoomingLogo.tsx
@@ -17,7 +17,7 @@ export default function ZoomingLogo() {
       gsap.to("#logo", {
         scrollTrigger: {
           trigger: "#logo",
-          start: "bottom bottom", // Start when the top of .logo hits the top of the viewport
+          start: "78% bottom", // Start when the top of .logo hits the top of the viewport
           end: `center top`, // End when the scroll position has moved by one viewport height
           scrub: true, // Sync animation with scroll position
         },
@@ -28,8 +28,8 @@ export default function ZoomingLogo() {
       gsap.to(".text-zoom", {
         scrollTrigger: {
           trigger: ".text-zoom",
-          start: `top center`, 
-          end: `50% 50%`, // End when the top of .text-zoom reaches 50% of the viewport height
+          start: `top 60%`, 
+          end: `50% 60%`, // End when the top of .text-zoom reaches 50% of the viewport height
           scrub: true, // Sync animation with scroll position
         },
         opacity: 1, // Fade in effect
@@ -48,18 +48,28 @@ export default function ZoomingLogo() {
     <>
       <div className="bg-[#070A0B] w-full h-[100vh]">
         <section className="main relative flex items-center justify-center w-full h-[100vh] overflow-hidden">
+
+        <video
+        className="absolute top-0 left-0 w-full h-full object-cover z-0"
+        autoPlay
+        muted
+        loop
+      >
+        <source src="/videos/ZOOM.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
           
           <Image
             id="logo"
-            src="/logos/logoo.svg" // You can also use import to get a local image.
+            src="/logos/ZOOM.svg" // You can also use import to get a local image.
             alt="Image 1"
-            width={350}
-            height={350}
-            className="logo origin-[50%_43%] relative"
+            layout="fill"
+            objectFit="cover" 
+            className="logo relative w-full h-full z-40"
           />
 
           <div className="absolute flex justify-center items-center text-center">
-            <h1 className="text-zoom text-3xl md:text-4xl w-[95%] md:w-[70%]">
+            <h1 className="text-zoom text-3xl md:text-4xl w-[95%] md:w-[70%] z-20">
               نحن شغوفون بالابتكار والأفكار الرائعة والتنفيذ الذي يجمع كل ذلك في تجربة واحدة جميلة. إذا كنت كذلك، فاتصل بنا أو أرسل لنا بريدًا إلكترونيًا للبدء.
             </h1>
           </div>
